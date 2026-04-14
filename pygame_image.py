f5afd88789a51b0c20d0bc4de0cfca91907dd9fb
tomokiakiyama <qiushantomoki257@gmail.com>
2026-04-14 15:47:41 +0900

    演習1：無入力時の左移動と右キー移動

--- pygame_image.py
+++ pygame_image.py
@@ -28,14 +28,17 @@ def main():
         screen.blit(bg_img, [-x + 3200, 0])
 
         key_lst = pg.key.get_pressed()
+        move_x = -1
+        move_y = 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            move_y -= 1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
+            move_y += 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            move_x -= 1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)
+            move_x += 1
+        kk_rct.move_ip(move_x, move_y)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
